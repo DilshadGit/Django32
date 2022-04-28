148b8bf8e4a3a5a0186fb3fdec4d0b2af669b1ec
DilshadGit <dilshad.a73@gmail.com>
2022-04-28 14:20:41 +0100

    Fixed small bug on the form in if statment.

--- django_32/article/forms.py
+++ django_32/article/forms.py
@@ -9,7 +9,6 @@ class ArticleForm(forms.Form):
     content = forms.CharField()
     # create_date = forms.DateTimeField()
 
-
     # def clean_title(self):
     #     cleaned_data = self.cleaned_data # dict
     #     print(cleaned_data, ' <<< cleaned data')
@@ -20,6 +19,7 @@ class ArticleForm(forms.Form):
     #     return title
 
     # this method will clean all data fields
+
     def clean(self):
         cleaned_data = self.cleaned_data
         print('All fields', cleaned_data)
@@ -28,7 +28,7 @@ class ArticleForm(forms.Form):
         if title.lower().strip() == 'python':
             self.add_error('title', 'This title is taken.')
             # raise form.ValidationError('This title is taken')
-        if 'python' in content or in title:
+        if 'python' in content or 'python' in title:
             self.add_error('content', 'Python can not be in the content')
             raise form.ValidationError('The article is already taken')
         return cleaned_data
