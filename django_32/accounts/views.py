a2db7cb42c97056f79520ebaaff2587b5dfc7407
DilshadGit <dilshad.a73@gmail.com>
2022-04-30 19:41:46 +0100

    Import AuthenticationForm from django.contrib.auth.forms import AuthenticationForm using for login_view only which is reduce the codes much more less and helping with templayes/login.html to use {{form.as_p}} instead of input fields.

--- django_32/accounts/views.py
+++ django_32/accounts/views.py
@@ -1,30 +1,47 @@
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.shortcuts import render, redirect
 
 # Create your views here.
 
-
 def user_login_view(request):
+    # future -> ?next=/articles/crete
     template_name = 'login.html'
     if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        print(username, password)
-        user = authenticate(request, username=username, password=password)
-        if user is None:
-            context = {
-                'error': 'Invalid username or password'
-            }
-            return render(request, template_name, context)
-        print(user)
-        login(request, user)
-        return redirect('/')
-    # context = {
-    #     'username': username,
-    #     'password': password
-    # }
-    return render(request, template_name, {})
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('/')
+    else:
+        form = AuthenticationForm(request)
+    context = {
+        'form': form
+    }
+    return render(request, template_name, context)
+
+# def user_login_view_v1(request):
+#     template_name = 'login.html'
+#     if request.method == 'POST':
+#         username = request.POST.get('username')
+#         password = request.POST.get('password')
+#         print(username, password)
+#         user = authenticate(request, username=username, password=password)
+#         if user is None:
+#             context = {
+#                 'error': 'Invalid username or password'
+#             }
+#             return render(request, template_name, context)
+#         print(user)
+#         login(request, user)
+#         return redirect('/')
+#     else:
+#         form = AuthenticationForm(request)
+#     # context = {
+#     #     'username': username,
+#     #     'password': password
+#     # }
+#     return render(request, template_name, {})
 
 
 def user_register_view(request):
