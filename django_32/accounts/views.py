a9706c5eb6368af963f3e758fcdd840809a12a92
DilshadGit <dilshad.a73@gmail.com>
2022-04-30 18:42:08 +0100

    Create user registeration form and saperate the pages when user loggedin and logged out.

--- django_32/accounts/views.py
+++ django_32/accounts/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 
 # Create your views here.
@@ -18,7 +19,7 @@ def user_login_view(request):
             return render(request, template_name, context)
         print(user)
         login(request, user)
-        return redirect('/admin')
+        return redirect('/')
     # context = {
     #     'username': username,
     #     'password': password
@@ -28,12 +29,19 @@ def user_login_view(request):
 
 def user_register_view(request):
     template_name = 'register.html'
-    return render(request, template_name, {})
+    form = UserCreationForm(request.POST or None)
+    if form.is_valid():
+        reg_form = form.save()
+        return redirect('accounts:login')
+    context = {
+        'form': form,
+    }
+    return render(request, template_name, context)
 
 
 def user_logout_view(request):
     template_name = 'logout.html'
     if request.method == 'POST':
     	logout(request)
-    	return redirect('/login/')
+    	return redirect('accounts:login')
     return render(request, template_name, {})
